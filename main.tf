58006195a3a9b778d18abd937586c5080d18c9f6
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-18 23:31:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,32 +9,54 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-# Virtual Network
+# Virtual Network (add depends_on + generous timeouts to avoid eventual consistency 404)
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-vnet"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
+
+  # Be explicit even though RG reference already implies this
+  depends_on = [azurerm_resource_group.rg]
+
+  timeouts {
+    create = "10m"
+    read   = "5m"
+    update = "10m"
+    delete = "10m"
+  }
 }
 
-# Subnet
+# Subnet (also give time to see the VNet after creation)
 resource "azurerm_subnet" "subnet" {
   name                 = "${var.prefix}-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+
+  timeouts {
+    create = "10m"
+    read   = "5m"
+    update = "10m"
+    delete = "10m"
+  }
 }
 
-# Public IP (use Standard SKU to avoid Basic quota limits)
+# Public IP (Standard SKU to avoid Basic quota limits)
 resource "azurerm_public_ip" "pip" {
   name                = "${var.prefix}-pip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   allocation_method   = "Static"
-  sku                 = "Standard"   # <- changed from Basic to Standard
-  # ip_version        = "IPv4"       # optional, default is IPv4
-  # sku_tier          = "Regional"   # optional, default is Regional
+  sku                 = "Standard"
+
+  timeouts {
+    create = "10m"
+    read   = "5m"
+    update = "10m"
+    delete = "10m"
+  }
 }
 
 # Network Security Group (allow SSH)
@@ -54,64 +76,12 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = var.allowed_ssh_cidr
     destination_address_prefix = "*"
   }
-}
 
-# Network Interface
-resource "azurerm_network_interface" "nic" {
-  name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "ipconfig1"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip.id
+  timeouts {
+    create = "10m"
+    read   = "5m"
+    update = "10m"
+    delete = "10m"
   }
 }
 
-# Associate NSG with NIC
-resource "azurerm_network_interface_security_group_association" "nic_nsg" {
-  network_interface_id      = azurerm_network_interface.nic.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-# Linux Virtual Machine (Ubuntu 22.04 LTS)
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "${var.prefix}-vm"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
-  admin_username      = var.admin_username
-  network_interface_ids = [azurerm_network_interface.nic.id]
-
-  # SSH only (no passwords)
-  disable_password_authentication = true
-
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = var.ssh_public_key
-  }
-
-  # ✅ Required in AzureRM v3.x
-  os_disk {
-    name                 = "${var.prefix}-osdisk"
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-    disk_size_gb         = 30
-  }
-
-  # Ubuntu 22.04 LTS (Jammy) Gen2
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
-    version   = "latest"
-  }
-
-  tags = {
-    Owner       = "Aishwarya"
-    Project     = "GitHub-Actions-VM"
-    Environment = "Dev"
-  }
-}
