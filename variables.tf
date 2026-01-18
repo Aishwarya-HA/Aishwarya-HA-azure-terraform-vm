f0e470827b0bea093f4084321cae49bd476f95d1
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-18 22:34:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@
 variable "location" {
   description = "Azure region for resources"
   type        = string
-  default     = "centralindia"
+  default     = "centralindia" # If capacity issues persist, change to 'southindia' or 'eastus'
 }
 
 variable "prefix" {
@@ -24,7 +24,14 @@ variable "allowed_ssh_cidr" {
 }
 
 variable "ssh_public_key" {
-  description = "Your SSH public key"
+  description = "Your SSH public key (MUST be RSA: starts with 'ssh-rsa')"
   type        = string
   sensitive   = true
+}
+
+# ✅ NEW: make VM size configurable; B2s is commonly available
+variable "vm_size" {
+  description = "Azure VM size (SKU). Change if the default is not available in your region."
+  type        = string
+  default     = "Standard_B2s"
 }
