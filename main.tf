6967e2433eddc91a7fc96ad37babcea6c94ba66e
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-18 23:21:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
-############################
-# Networking & Compute
-############################
+#############################################
+# Networking + VM for Ubuntu 22.04 (Jammy)
+#############################################
 
 # Resource Group
 resource "azurerm_resource_group" "rg" {
@@ -25,25 +25,23 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Public IP (Standard SKU to avoid Basic SKU quota limits)
+# Public IP
 resource "azurerm_public_ip" "pip" {
   name                = "${var.prefix}-pip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
-  allocation_method   = "Static"    # Standard requires Static
-  sku                 = "Standard"
-  ip_version          = "IPv4"
+  allocation_method   = "Static"
+  sku                 = "Basic"
 }
 
-# Network Security Group (SSH only; lock down later via allowed_ssh_cidr)
+# Network Security Group (allow SSH)
 resource "azurerm_network_security_group" "nsg" {
   name                = "${var.prefix}-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "Allow-SSH"
+    name                       = "Allow-SSH-22"
     priority                   = 1001
     direction                  = "Inbound"
     access                     = "Allow"
@@ -55,7 +53,7 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
-# NIC
+# Network Interface
 resource "azurerm_network_interface" "nic" {
   name                = "${var.prefix}-nic"
   location            = azurerm_resource_group.rg.location
@@ -69,45 +67,49 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-# Attach NSG to NIC
+# Associate NSG with NIC (recommended with VM v3 resources)
 resource "azurerm_network_interface_security_group_association" "nic_nsg" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-# Linux VM (Ubuntu 22.04 LTS)
+# Linux Virtual Machine (Ubuntu 22.04 LTS)
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "${var.prefix}-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  network_interface_ids = [azurerm_network_interface.nic.id]
 
-  # Use variable so we can change size if SKU isn't available in the region
-  size           = var.vm_size
-  admin_username = var.admin_username
-
-  network_interface_ids = [
-    azurerm_network_interface.nic.id
-  ]
-
+  # Use SSH only (no passwords)
   disable_password_authentication = true
 
   admin_ssh_key {
     username   = var.admin_username
-    # IMPORTANT: must be an RSA public key (ssh-rsa ...)
     public_key = var.ssh_public_key
   }
 
-  # Required with AzureRM v3.x
+  # ✅ REQUIRED with AzureRM v3.x: define OS disk explicitly
   os_disk {
     name                 = "${var.prefix}-osdisk"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
+    disk_size_gb         = 30
   }
 
+  # Ubuntu 22.04 LTS (Jammy) Gen2 image
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts-gen2"
     version   = "latest"
+  }
+
+  # Optional tags (helpful for cost & identification)
+  tags = {
+    Owner       = "Aishwarya"
+    Project     = "GitHub-Actions-VM"
+    Environment = "Dev"
   }
 }
