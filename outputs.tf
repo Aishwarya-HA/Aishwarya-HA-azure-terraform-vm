976c8666fbca884416f537dc07a3100268ec9a19
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-19 16:56:33 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,7 @@
 
-output "resource_group_name" {
-  value = azurerm_resource_group.example.name
-}
-
-output "vnet_name" {
-  value = azurerm_virtual_network.main.name
-}
-
-output "subnet_id" {
-  value = azurerm_subnet.internal.id
-}
-
-output "nic_id" {
-  value = azurerm_network_interface.main.id
-}
-
-output "vm_id" {
-  value = azurerm_linux_virtual_machine.main.id
-}
-
-output "private_ip" {
-  value = azurerm_network_interface.main.ip_configuration[0].private_ip_address
-}
+output "resource_group_name" { value = azurerm_resource_group.example.name }
+output "vnet_name"           { value = azurerm_virtual_network.main.name }
+output "subnet_id"           { value = azurerm_subnet.internal.id }
+output "nic_id"              { value = azurerm_network_interface.main.id }
+output "vm_id"               { value = azurerm_linux_virtual_machine.main.id }
+output "private_ip"          { value = azurerm_network_interface.main.ip_configuration[0].private_ip_address }
