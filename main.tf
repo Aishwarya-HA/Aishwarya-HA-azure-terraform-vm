a010eca153ec0f43561fc562fe97a17c56f411de
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-19 17:10:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,27 @@
 
+#############################################
+# Resource Group
+#############################################
+
 resource "azurerm_resource_group" "example" {
   name     = "${var.prefix}-resources"
   location = var.location
 }
 
+#############################################
+# Virtual Network
+#############################################
+
 resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-vnet-new"
+  name                = "${var.prefix}-vnet-new"   # New name to avoid any prior ghost VNet issues
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
+
+#############################################
+# Subnet
+#############################################
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
@@ -18,45 +30,10 @@ resource "azurerm_subnet" "internal" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+#############################################
+# Network Interface (NIC)
+#############################################
+
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
   location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "ipconfig1"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "main" {
-  name                = "${var.prefix}-vm"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  size                = "Standard_B2ms"
-  admin_username      = var.admin_username
-
-  network_interface_ids = [azurerm_network_interface.main.id]
-
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = var.admin_ssh_public_key
-  }
-
-  disable_password_authentication = true
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-
-  tags = { environment = "staging" }
-}
