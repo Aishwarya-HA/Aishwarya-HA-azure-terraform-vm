105dca8be0e4e752bb7288861c3e1ff579f74b53
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-19 12:16:27 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,15 +1,20 @@
 
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.6.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.100"
+      version = "~> 3.120"
     }
   }
 
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state-aish"
+    storage_account_name = "tfstateaish21877"
+    container_name       = "tfstate"
+    key                  = "dev.tfstate"
+  }
 }
 
 provider "azurerm" {
