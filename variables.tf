bbdb905c45e1f27140f4338dbb66310bc79d02af
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-19 18:52:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,14 +3,16 @@
 # Global Variables
 ###############################
 
+# Used as a prefix in names of RG, VNet, NIC, VM, etc.
 variable "prefix" {
-  description = "Resource naming prefix"
+  description = "Resource naming prefix used for all Azure resources"
   type        = string
   default     = "tfvmex"
 }
 
+# Target Azure region for all resources
 variable "location" {
-  description = "Azure region for deployment"
+  description = "Azure region for deployment (e.g., Central India, South India)"
   type        = string
   default     = "Central India"
 }
@@ -19,13 +21,15 @@ variable "location" {
 # VM Access Variables
 ###############################
 
+# Safe admin username for the Linux VM (avoid reserved names like admin, root, azureuser)
 variable "admin_username" {
-  description = "Admin username for Linux VM"
+  description = "Admin username for the Linux VM"
   type        = string
   default     = "vmadmin"
 }
 
+# SSH public key to log in (pass via TF_VAR_admin_ssh_public_key in the workflow)
 variable "admin_ssh_public_key" {
-  description = "SSH public key for VM login"
+  description = "SSH public key for VM login (contents of your .pub key)"
   type        = string
 }
