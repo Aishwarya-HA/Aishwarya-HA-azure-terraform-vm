a1afc384045c49ba6162dacfd98a419022eb76f7
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-19 15:59:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,38 @@
 
+###############################
+# Global Variables
+###############################
+
+# Prefix used for naming all resources
 variable "prefix" {
   description = "Resource name prefix"
   type        = string
   default     = "tfvmex"
 }
 
+# Azure deployment region
 variable "location" {
-  description = "Azure region"
+  description = "Azure region for all resources"
   type        = string
-  default     = "Central India"   # ✔ Works in your subscription
+  default     = "Central India"   # ✔ Works reliably
 }
 
+###############################
+# VM Access Variables
+###############################
+
+# Admin username for the VM
+# NOTE: Azure blocks some usernames like 'azureuser', 'admin', 'testadmin', 'root'
+# 'vmadmin' is safe and fully supported on Linux VMs
 variable "admin_username" {
-  description = "Admin username for the VM"
+  description = "Admin username for the Linux VM"
   type        = string
-  default     = "azureuser"
+  default     = "vmadmin"         # ✔ Updated to avoid Azure restrictions
 }
 
+# SSH public key for login to the VM
+# This is passed via GitHub Actions secret: SSH_PUBLIC_KEY
 variable "admin_ssh_public_key" {
-  description = "SSH public key for VM login"
+  description = "SSH public key for VM login (from SSH keypair)"
   type        = string
 }
