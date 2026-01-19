c929bd50c56262693b196b506796167bf0699cc9
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-19 12:25:13 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,11 @@
 
-# providers.tf
 terraform {
   required_version = ">= 1.6.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.120"
+      version = ">= 3.0, < 4.0"
     }
   }
 
