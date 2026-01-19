af4e3e0226e20e7f276832e37543ded9d7ae8ca6
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-19 12:27:19 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,16 @@
 
-output "public_ip" {
-  description = "The public IP of the virtual machine"
-  value       = azurerm_public_ip.pip.ip_address
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name
 }
 
-output "ssh_command" {
-  description = "Use this command to SSH into the VM"
-  value       = "ssh azureuser@${azurerm_public_ip.pip.ip_address}"
+output "vm_id" {
+  value = azurerm_linux_virtual_machine.main.id
+}
+
+output "nic_id" {
+  value = azurerm_network_interface.main.id
+}
+
+output "private_ip" {
+  value = azurerm_network_interface.main.ip_configuration[0].private_ip_address
 }
